a438604e915df0c5b33011b6963c028803b73ad4
Paweł Gross <pawel.gross@grupawp.pl>
2024-03-11 15:28:43 +0100

    test finish game

--- score_board/score_board_test.go
+++ score_board/score_board_test.go
@@ -11,3 +11,14 @@ func TestStartGame(t *testing.T) {
 		t.Errorf("Summary should contain one match after starting a game, got: %d", len(summary))
 	}
 }
+
+func TestFinishGame(t *testing.T) {
+	sb := ScoreBoard{}
+	sb.StartGame("TeamA", "TeamB")
+	sb.FinishGame("TeamA", "TeamB")
+
+	summary := sb.Summary()
+	if len(summary) != 0 {
+		t.Errorf("Summary should be empty after finishing the game, got: %d", len(summary))
+	}
+}
