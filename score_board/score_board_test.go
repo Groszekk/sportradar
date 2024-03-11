cfe73be35efb96497ae88bd30c0122c57d36b89e
Groszekk <pawel.gross@grupawp.pl>
2024-03-11 16:39:43 +0100

    test update score

--- score_board/score_board_test.go
+++ score_board/score_board_test.go
@@ -22,3 +22,17 @@ func TestFinishGame(t *testing.T) {
 		t.Errorf("Summary should be empty after finishing the game, got: %d", len(summary))
 	}
 }
+
+func TestUpdateScore(t *testing.T) {
+	sb := ScoreBoard{}
+	sb.StartGame("TeamA", "TeamB")
+	sb.UpdateScore("TeamA", "TeamB", 2, 1)
+
+	summary := sb.Summary()
+	if summary[0].HomeScore != 2 {
+		t.Errorf("Home team score should be updated, got: %d", summary[0].HomeScore)
+	}
+	if summary[0].AwayScore != 1 {
+		t.Errorf("Away team score should be updated, got: %d", summary[0].AwayScore)
+	}
+}
