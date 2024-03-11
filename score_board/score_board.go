dfe191def2512d644a0cb88cf43a115395914faf
Paweł Gross <pawel.gross@grupawp.pl>
2024-03-11 14:56:29 +0100

    start game

--- score_board/score_board.go
+++ score_board/score_board.go
@@ -5,3 +5,13 @@ import "sportradar/models"
 type ScoreBoard struct {
 	matches []*models.Match
 }
+
+func (sb *ScoreBoard) StartGame(homeTeam, awayTeam string) {
+	match := &models.Match{
+		HomeTeam:  homeTeam,
+		AwayTeam:  awayTeam,
+		HomeScore: 0,
+		AwayScore: 0,
+	}
+	sb.matches = append(sb.matches, match)
+}
